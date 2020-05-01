1bedd891cf54422caee19e7b53e504fb1ae3631c
MiguelMata <nelsonmgarciam@gmail.com>
2020-05-02 01:22:20 +0200

    fist commit

---
+++ exercises/1/unicorn.js
@@ -0,0 +1,25 @@
+class Unicorn {
+  constructor(name, color = "white") {
+    this._name = name;
+    this._color = color;
+  }
+
+  get name(){
+    return this._name;
+  }
+
+  get color(){
+    return this._color;
+  }
+
+  isWhite(){
+    return this._color === "white";
+  }
+
+  says(string){
+    return `**;* ${string} *;**`;
+  }
+
+}
+
+module.exports = Unicorn;
